8b93602335c8eff4a6625654dfce6edeeeb91066
Pavel Safonov <p.n.safonov@gmail.com>
2020-08-17 12:25:04 +0300

    test

--- src/tests/test_1/test_1.ts
+++ src/tests/test_1/test_1.ts
@@ -4,7 +4,7 @@ import isOdd = require("is-odd");
 function test1_is_odd(): void {
     const r1 = isOdd(111);
 
-    console.log(r1);
+    console.log("r1 = %s, r1 = %d", r1, r1);
 }
 
 function run_tests_1(): boolean {
